3ff673f24544a2aecc14eec4652bf6b20bb4720f
StavrosTsoukalas <bluestacksonly50@gmail.com>
2022-02-11 19:22:40 +0200

    Create a style.js file and add js to create posts' style

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../js/bootstrap_js_files.js'
+import '../posts/style.js'
 
 Rails.start()
 Turbolinks.start()
