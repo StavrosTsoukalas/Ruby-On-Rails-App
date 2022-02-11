b9bbea2f43d0ea15c6e15164908d792ad54a5f58
StavrosTsoukalas <bluestacksonly50@gmail.com>
2022-02-11 14:28:27 +0200

    - Generate Post and Category models. - Create associations between User, Post and Category models. - Create categories and posts database tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_174421) do
+ActiveRecord::Schema.define(version: 2022_02_11_121605) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "branch"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
