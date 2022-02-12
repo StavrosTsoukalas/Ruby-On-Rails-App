d9981ccc834ae604bfd90ef82397c31dfc928501
StavrosTsoukalas <bluestacksonly50@gmail.com>
2022-02-12 14:59:18 +0200

    Make search bar and category filters in branch pages functional - Add by_category, by_branch and search scopes in the Post model - Modify the get_posts method in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,19 @@ class PostsController < ApplicationController
     @posts = get_posts.paginate(page: params[:page])
   end
   def get_posts
-    Post.limit(30)
+    branch = params[:action]
+    search = params[:search]
+    category = params[:category]
+
+    if category.blank? && search.blank?
+      posts = Post.by_branch(branch).all
+    elsif category.blank? && search.present?
+      posts = Post.by_branch(branch).search(search)
+    elsif category.present? && search.blank?
+      posts = Post.by_category(branch, category)
+    elsif category.present? && search.present?
+      posts = Post.by_category(branch, category).search(search)
+    else
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
   default_scope -> { includes(:user).order(created_at: :desc) }
+  scope :by_category, -> (branch, category_name) do 
+    joins(:category).where(categories: {name: category_name, branch: branch}) 
+  end
+
+  scope :by_branch, -> (branch) do
+    joins(:category).where(categories: {branch: branch}) 
+  end
+
+  scope :search, -> (search) do
+    where("title LIKE lower(?) OR content LIKE lower(?)", "%#{search}%", "%#{search}%")
+  end
 end
