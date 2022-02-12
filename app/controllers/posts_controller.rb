6812537eef9af32a94c0436e60d1dee7177801d1
StavrosTsoukalas <bluestacksonly50@gmail.com>
2022-02-12 21:37:11 +0200

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
   def show
     @post = Post.find(params[:id])
   end
@@ -14,6 +15,29 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
   
+  def get_posts
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
+  end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
+
   private
   def posts_for_branch(branch)
     @categories = Category.where(branch: branch)
@@ -23,12 +47,8 @@ class PostsController < ApplicationController
       format.js { render partial: 'posts/posts_pagination_page' }
     end
   end
-
-  def get_posts
-    PostsForBranchService.new({
-      search: params[:search],
-      category: params[:category],
-      branch: params[:action]
-    }).call
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                       .merge(user_id: current_user.id)
   end
 end
