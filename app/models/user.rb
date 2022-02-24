dab0fbf1123fde7e60a017457410059893dcaef2
StavrosTsoukalas <bluestacksonly50@gmail.com>
2022-02-24 19:58:36 +0200

    Add associations and helper methods to the User model - Create relationship between the User the Contact models - Methods help query the Contact records

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,38 @@ class User < ApplicationRecord
   has_many :private_conversations, 
           foreign_key: :sender_id, 
           class_name: 'Private::Conversation'
+
+  has_many :contacts
+  has_many :all_received_contact_requests,  
+          class_name: "Contact", 
+          foreign_key: "contact_id"
+
+  has_many :accepted_sent_contact_requests, -> { where(contacts: { accepted: true}) }, 
+          through: :contacts, 
+          source: :contact
+  has_many :accepted_received_contact_requests, -> { where(contacts: { accepted: true}) }, 
+          through: :all_received_contact_requests, 
+          source: :user
+  has_many :pending_sent_contact_requests, ->  { where(contacts: { accepted: false}) }, 
+          through: :contacts, 
+          source: :contact
+  has_many :pending_received_contact_requests, ->  { where(contacts: { accepted: false}) }, 
+          through: :all_received_contact_requests, 
+          source: :user
+
+  # gets all your contacts
+  def all_active_contacts
+    accepted_sent_contact_requests | accepted_received_contact_requests
+  end
+
+  # gets your pending sent and received contacts
+  def pending_contacts
+    pending_sent_contact_requests | pending_received_contact_requests
+  end
+
+  # gets a Contact record
+  def contact(contact)
+    Contact.where(user_id: self.id, contact_id: contact.id)[0]
+  end
+
 end
