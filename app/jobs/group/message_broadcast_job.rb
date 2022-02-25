c68bd953d7c11b964377ec99545be86dc6a7903d
StavrosTsoukalas <bluestacksonly50@gmail.com>
2022-02-25 22:53:52 +0200

    Create a NotificationChannel

--- app/jobs/group/message_broadcast_job.rb
+++ app/jobs/group/message_broadcast_job.rb
@@ -4,12 +4,7 @@ class Group::MessageBroadcastJob < ApplicationJob
   def perform(message, previous_message, current_user)
     # broadcast message to all conversation's participants
     conversation_id = message.conversation_id
-    ActionCable.server.broadcast(
-      "group_conversation_#{conversation_id}",
-      message: render_message(message, previous_message),
-      conversation_id: conversation_id,
-      user_id: message.user_id
-    )
+    ActionCable.server.broadcast("group_conversation_#{conversation_id}", {message: render_message(message, previous_message), conversation_id: conversation_id, user_id: message.user_id})
   end
 
   def render_message(message, previous_message)
